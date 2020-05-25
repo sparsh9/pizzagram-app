8461b8059af393691e20ef5b94944f870edb125d
sparsh9 <sparshgarg2000@outlook.com>
2020-05-25 15:28:31 +0530

    pay-button-added

--- src/components/Cart/CartTotals.js
+++ src/components/Cart/CartTotals.js
@@ -41,7 +41,11 @@ export default class CartTotals extends Component {
                                     <span className='text-title'> total :</span>{' '}
                                     <strong>$ {cartTotal} </strong>
                                 </h5>
-
+                                <PayButton
+                                    totalAmount={cartTotal}
+                                    clearCart={clearCart}
+                                    history={history}
+                                />
                             </div>
                         </div>
                     </div>
